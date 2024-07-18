eb215387a15f6503179d9d4d18c9262a4a9a92e0
Axel Derail Santer <115737371+siryo-ax@users.noreply.github.com>
2024-07-18 13:20:54 +0200

    Proper extraction of new smells when generating examples from smell tracker

--- data_extraction/arcan/smell_tracker/generate_examples.py
+++ data_extraction/arcan/smell_tracker/generate_examples.py
@@ -14,13 +14,19 @@ def generate_examples(smell_tracker: list, number_of_examples=10):
     examples: list = []
 
     for s in smell_tracker:
-        if len(s["characteristics_by_version"]) > 1 and (
-                s["characteristics_by_version"][1]["ATDI_var"]["variation"] == "UP" or
-                s["characteristics_by_version"][1]["ATDI_var"]["variation"] == "DOWN" or
-                s["characteristics_by_version"][1]["ATDI_var"]["variation"] == "NEW"):
+        if detect_var(s):
             examples.append(s)
             number_of_examples -= 1
             if number_of_examples == 0:
                 break
 
     return examples
+
+
+def detect_var(smell):
+    if len(smell["characteristics_by_version"]) > 1:
+        return smell["characteristics_by_version"][1]["ATDI_var"]["variation"] == "UP" or \
+            smell["characteristics_by_version"][1]["ATDI_var"]["variation"] == "DOWN"
+    else:
+        if "ATDI_var" in smell["characteristics_by_version"][0]:
+            return smell["characteristics_by_version"][0]["ATDI_var"]["variation"] == "NEW"
